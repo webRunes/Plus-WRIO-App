b7c0d06e8be61143072fb4e8ba8a430ad2146084
bhushano7 <bhushan2250@gmail.com>
2014-11-30 00:48:34 +0530

    update google login

---
+++ app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = require("./wrio_app.js").init(express);
+var server = require('http').createServer(app).listen(1234);
+
+var passport = require('passport');
+var GooglePlusStrategy = require('passport-google-oauth').OAuth2Strategy;
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var nconf = require("./wrio_nconf.js").init();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(session(
+	{
+		secret: 'keyboard cat',
+		saveUninitialized: true,
+		resave: true,
+		key: 'sid'
+	}
+));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+	done(null, obj);
+});
+
+passport.use(new GooglePlusStrategy({
+		clientID: nconf.get('api:google:clientId'),
+		clientSecret: nconf.get('api:google:clientSecret'),
+		callbackURL: nconf.get('api:google:callbackUrl')
+	},
+	function (accessToken, refreshToken, profile, done) {
+		process.nextTick(function () {
+			return done(null, profile);
+		});
+	}
+));
+
+app.get('/', function (request, response) {
+	console.log(request.user);
+	response.render('index', {user: request.user});
+});
+
+app.get('/account', ensureAuthenticated, function (request, response) {
+	response.render('account', {user: request.user});
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['https://www.googleapis.com/auth/userinfo.profile',
+		'https://www.googleapis.com/auth/userinfo.email']
+}));
+
+app.get('/auth/google/callback',
+	passport.authenticate('google', {successRedirect: '/', failureRedirect: '/login'}),
+	function (request, response) {
+		response.redirect('/');
+	});
+
+app.get('/logout', function (request, response) {
+	request.logout();
+	response.redirect('/');
+});
+
+function ensureAuthenticated(request, response, next) {
+	if (request.isAuthenticated()) {
+		return next();
+	}
+	response.redirect('/login')
+}
