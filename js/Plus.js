8086cbd949bdfb9073d9d9c7186f3dc6f7a8f61c
Vladimir <spirin.vladimir@gmail.com>
2015-06-08 17:02:04 +0300

    plus

--- js/Plus.js
+++ js/Plus.js
@@ -1,11 +1,14 @@
-var React = require('react'),
+var React = require('react/addons'),
     Reflux = require('reflux'),
     storage = require('./storages/jsonld'),
     actions = require('./actions/jsonld');
 
-var Plus = React.createClass({
+ var Plus = React.createClass({
     mixins: [Reflux.connect(storage, 'jsonld')],
     render: function() {
+        if (this.state === null) {
+            return null;
+        }
         return (
             <div className="navbar-collapse in">
                 <div className="navbar-header" id="leftMenuwrp">
@@ -21,22 +24,34 @@ var List = React.createClass({
         data: React.PropTypes.object.isRequired
     },
     render: function() {
-        var lis = Object.keys(this.props.data).map(function (name, key) {
-            var item = this.props.data[name]; 
+        var lis = Object.keys(this.props.data).map(function (name) {
+            var item = this[name];
             if (Object.prototype.toString.call(item) === '[object Array]') {
-                return (
-                    <li>
-                        <SubList data={item} name={name} key={key} />
-                    </li>
-                );
+                return <SubList data={item} listName={name} key={name} />;
             } else {
-                return <Element data={item} listName={name} key={key} />;
+                return <Element data={item} listName={name} key={name} />;
             }
-        }, this);
+        }, this.props.data);
         return (
             <ul id="nav-accordion" className="nav navbar-var">
                 {lis}
+                <P />
             </ul>
+        );
+    }
+});
+
+var P = React.createClass({
+    link: function () {
+        window.location = 'http://wrioos.com';
+    },
+    render: function() {
+        return (
+            <li className="new panel">
+                <a onClick={this.link}>
+                    <span className="glyphicon glyphicon-plus"></span>
+                </a>
+            </li>
         );
     }
 });
@@ -44,16 +59,34 @@ var List = React.createClass({
 var SubList = React.createClass({
     propTypes: {
         data: React.PropTypes.array.isRequired,
-        name: React.PropTypes.string.isRequired
+        listName: React.PropTypes.string.isRequired
+    },
+    getInitialState: function() {
+        return {
+            style: {
+                height: 'auto',
+                overflow: 'hidden'
+            }
+        };
+    },
+    toggle: function () {
+        this.setState(React.addons.update(this.state, {
+            style: {
+                height: {$set: (this.state.style.height === 'auto') ? '0px' : 'auto'}
+            }
+        }));
     },
     render: function() {
         var lis = this.props.data.map(function (item, key) {
-            return <Element data={item} listName={this.props.name} key={key} />;
+            return <Element data={item} listName={this.props.listName} key={key} />;
         }, this);
         return (
             <li className="panel">
-                <a href="#element2" className="" data-parent="#nav-accordion" data-toggle="collapse"><span className="qty btn btn-xs pull-right">{lis.length}</span>{this.props.name}</a>
-                <div className="in" style="height: auto;">
+                <a onClick={this.toggle}>
+                    <span className="qty pull-right">{lis.length}</span>
+                    <span>{this.props.listName}</span>
+                </a>
+                <div className="in" style={this.state.style}>
                     <ul className="nav nav-pills nav-stacked sub">
                         {lis}
                     </ul>
@@ -71,14 +104,17 @@ var Element = React.createClass({
     delete: function () {
         actions.delete(this.props.listName, this.props.data);
     },
+    link: function () {
+        window.location = this.props.data.url;
+    },
     render: function() {
         var o = this.props.data;
         return (
-            <li>
+            <li onClick={this.link}>
                 <a onClick={this.delete} className="pull-right">
                     <span className="glyphicon glyphicon-remove" />
                 </a>
-                <a href={o.url}>{o.name}</a>
+                <a onClick={this.link}>{o.name}</a>
             </li>
         );
     }
---
+++ test/index.js
@@ -0,0 +1,7 @@
+var domready = require('domready'),
+	React = require('react'),
+	Plus = require('../index');
+
+domready(function () {
+	React.render(<Plus />, document.body);
+});
