26c445529c25d3ecff5e9a7b71d9d67eb158e319
savitskayads <dariasav91@mail.ru>
2015-09-11 02:45:05 +0300

    Plus update style for nested tabs

--- js/P.js
+++ js/P.js
@@ -15,14 +15,14 @@ var P = React.createClass({
             active: !this.state.active
         });
     },
-    gotoUlr: function(){
-        window.location = 'http://wr.io/' + this.props.data.url + '/Plus-WRIO-App/';
+    gotoUrl: function(){
+        //window.location = 'http://' + this.props.data.url;
     },
     render: function () {
         var className = classNames('new panel', {active: this.state.active});
         return (
             <li className={className}>
-                <a onClick={function() { this.active(); this.gotoUrl(); }} style={{width: '100%'}} className="collapsed">
+                <a onClick={this.gotoUrl} style={{width: '100%'}} className="collapsed">
                     <span className="glyphicon glyphicon-plus"></span>
                 </a>
             </li>
